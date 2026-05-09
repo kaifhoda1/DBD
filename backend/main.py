22abfd47f862e32964106479e22e8d32529075f2
kaifhoda1 <kaifhoda1@gmail.com>
2026-05-09 08:18:25 +0530

    fix CORS for production

--- backend/main.py
+++ backend/main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173", "https://resplendent-pixie-4ecde8.netlify.app"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
